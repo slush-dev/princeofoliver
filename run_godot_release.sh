581d9677a3d46876a7ccd64f528c6dbcea644076
slush-dev <marekp2310@gmail.com>
2026-02-04 06:17:21 +0000

    Move Godot project under godot/

--- run_godot_release.sh
+++ run_godot_release.sh
@@ -7,6 +7,6 @@ XDG_CONFIG_HOME="/home/dev/projekty/princeofoliver/.xdg/config" \
 XDG_CACHE_HOME="/home/dev/projekty/princeofoliver/.xdg/cache" \
 XDG_DATA_HOME="/home/dev/projekty/princeofoliver/.xdg/data" \
 "/home/dev/projekty/princeofoliver/tools/Godot_v4.6-stable_linux.x86_64" \
-  --path "/home/dev/projekty/princeofoliver" \
+  --path "/home/dev/projekty/princeofoliver/godot" \
   --rendering-driver opengl3 \
   --scene "res://scenes/Main.tscn"
